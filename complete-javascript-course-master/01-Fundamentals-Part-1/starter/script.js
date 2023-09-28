8da83d4ab8046f8c7c80cae212addc0e5fcf9654
lpnam <kgzwhite00@gmail.com>
2023-09-28 22:56:33 +0700

    first commit

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,107 @@
+let js = 'amazing2';
+if (js === 'amazing') alert('Javascript is FUN');
+
+console.log(40+8+23-10);
+
+console.log('Jonas2');
+console.log(23);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let _function = 27;
+let $new = "new";
+
+let person = 'jonas';
+let PI = 3.1415;
+
+let myFirstJob = "Tester";
+let myCurrentJob = "Coder"
+
+/////////////////PracTice///////////////////////
+// LECTURE: Values and Variables
+let country = "VietNam";
+let continent = "Asian";
+let population = 97470000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+// LECTURE: Data Types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+// LECTURE: let, const and var
+
+language = "Vietnamese"
+const NUMBER = 9;
+// NUMBER = 10; Assignment to constant variable.
+
+// LECTURE: Basic Operators
+let halfOfPopulation = population/2;
+console.log(halfOfPopulation);
+let populationPlusOne = population + 1;
+console.log(populationPlusOne);
+
+const POPULATION_FINLAND = 6000000;
+if(population > POPULATION_FINLAND) console.log("Bigger than POPULATION_FINLAND");
+else console.log("Smaller");
+
+const EVERAGE_POPULATION = 33000000;
+if(population > EVERAGE_POPULATION) console.log(`'${country}'s population is above average`);
+else console.log(`'${country}'s population is ${(EVERAGE_POPULATION-population)/1000000} million below average`);
+
+let description = country + " is in " + continent + ", and its " + population/1000000 
++ " million people speak " + language;
+console.log(description);
+
+// LECTURE: Strings and Template Literals
+description = `${country} is in ${continent}, and its ${population/1000000} million people speak ${language}`;
+console.log(description);
+// LECTURE: Taking Decisions: if / else Statements
+// WORK At LECTURE: Basic Operators
+// LECTURE: Type Conversion and Coercion
+console.log('9' - '5');//4
+console.log('19' - '13' + '17');//23
+console.log('19' - '13' + 17);//23
+console.log('123' < 57);//False
+console.log(5 + 6 + '4' + 9 - 4 - 2);//18
+// LECTURE: Equality Operators: == vs. ===
+// let numNeighbours = prompt("How many neighbour countries does your country have?");
+
+// if (numNeighbours == 1) console.log('Only 1 border!');
+// else if (numNeighbours>1) console.log('More than 1 border');
+// else console.log('No borders');
+// LECTURE: Logical Operators
+if(language === 'english' && population<50000000 && !isIsland){
+    console.log(`You should live in ${country} :)`);
+}
+else console.log(`${country} does not meet your criteria :(`);
+// LECTURE: The switch Statement
+
+switch(language) {
+    case 'chinese':
+    case 'mandarin':
+      console.log('MOST number of native speakers!');
+      break;
+    case 'spanish':
+      console.log('2nd place in number of native speakers');
+      break;
+    case 'english':
+      console.log('3rd place');
+      break;
+    case 'hindi':
+      console.log('Number 4');
+      break;
+    default:
+      console.log('Great language too :D');
+  }
+//   LECTURE: The Conditional (Ternary) Operator
+// Done
