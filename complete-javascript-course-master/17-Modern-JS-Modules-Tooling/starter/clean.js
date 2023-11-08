aac3aee4e65f81faae7b7ce3b9129597c337d345
lpnam <kgzwhite00@gmail.com>
2023-11-08 23:35:06 +0700

    finish clean code section

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/clean.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,4 +1,5 @@
-var budget = [
+'strict mode';
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -7,58 +8,77 @@ var budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
-var limits = {
+const spendingLimits = Object.freeze({
+  //Immute
   jonas: 1500,
   matilda: 100,
+});
+// spendingLimits.jay = 200;
+// console.log(spendingLimits);
+
+const getLimit = (limits, user) => limits?.[user] ?? 0;
+
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = user.toLowerCase();
+
+  // let lim = 0;
+  // if (spendingLimits[user]) {
+  //   lim = spendingLimits[user];
+  // }
+
+  // const limit = spendingLimits[user] ? spendingLimits[user] : 0;
+
+  // const limit = getLimit(user);
+
+  return value <= getLimit(limits, cleanUser)
+    ? [...state, { value: -value, description: description, user: cleanUser }]
+    : [...state];
 };
-
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
-  user = user.toLowerCase();
-
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
-
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
-  }
-};
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
+const newBudget1 = addExpense(budget, spendingLimits, 10, 'Pizza 🍕');
+const newBudget2 = addExpense(
+  newBudget1,
+  spendingLimits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, spendingLimits, 200, 'Stuff', 'Jay');
 console.log(budget);
+console.log(newBudget3);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
+// budget[0].value = 1210;
+// budget[9] = 'Jonnn';
 
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
-};
-check();
+const checkExpenses = (state, limits) =>
+  state.map(entry =>
+    entry.value < -getLimit(limits, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry
+  );
+// for (const entry of state) {
+//   if (entry.value < -getLimit(limits, entry.user)) {
+//     entry.flag = 'limit';
+//   }
+// }
 
-console.log(budget);
+const finalBudget = checkExpenses(newBudget3, spendingLimits);
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
-};
+console.log(finalBudget);
+
+const logBigExpenses = (state, bigLimit) =>
+  state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+// .reduce((str, cur) => `${str} / ${cur.description.slice(-2)}`, '');
+
+const final = logBigExpenses(finalBudget, 1000);
+console.log(final);
--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -9,12 +9,17 @@ console.log('Importing module');
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart.totalPrice, ShoppingCart.tq);
 
-// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-import add, { cart } from './shoppingCart.js';
+import add, {
+  addToCart,
+  totalPrice as price,
+  tq,
+  cart,
+} from './shoppingCart.js';
 
 add('pizza', 6);
 add('bread', 5);
 add('aple', 4);
+add('momo', 4);
 
 // console.log(cart);
 // console.log('Something1');
@@ -30,12 +35,78 @@ const getLastPost = async function () {
 
   return { title: data.at(-1).title, text: data.at(-1).body };
 };
-const lastPost = getLastPost();
+// const lastPost = getLastPost();
 // console.log(lastPost);
 
 //Not clean
-lastPost.then(res => console.log(res));
+// lastPost.then(res => console.log(res));
 
-// clean way - top level await
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// // clean way - top level await
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+/*
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2.cart);
+*/
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 2 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+
+console.log(stateClone);
+
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateDeepClone);
+console.log('cl');
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+class Person {
+  greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
+}
+const jonas = new Person('Jonas');
+
+console.log('Jonas' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+// import 'core-js/stable';
+import 'core-js/stable/array/find.js';
